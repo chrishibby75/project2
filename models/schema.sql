dfa6570f99318dbd9edce2421198782d2cd2ab67
chrishibby75 <chrishibby75@gmail.com>
2018-12-08 12:31:13 -0700

    start game screens

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,21 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+CREATE DATABASE rpgame;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE game(
+    id INT AUTO INCREMENT NOT NULL,
+    game_name VARCHAR(140) NOT NULL,
+    password VARCHAR(24) NOT NULL,
+    area INT DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE character(
+    id INT AUTO INCREMENT NOT NULL,
+    character_name VARCHAR(100) NOT NULL,
+    gold INT DEFAULT 2000,
+    assets INT DEFAULT 0,
+    potion INT DEFAULT 0,
+    food INT DEFAULT 0,
+    PRIMARY KEY(id),
+    FOREIGN KEY(character.id),
+);
+
