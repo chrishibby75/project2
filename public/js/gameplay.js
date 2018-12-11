da878a7fb8571b77ca6de61dd84ac160215258af
Max Wilets <mwilets@gmail.com>
2018-12-11 15:12:09 -0700

    api checks

--- public/js/gameplay.js
+++ public/js/gameplay.js
@@ -8,89 +8,89 @@ var dialogue = [
     "You escaped the bandits and grabbed 300 gold!",
     "The bandits beat you up you lost 40 HP",
     "What do you want to do?"
-  ];
+];
 
-  ///front end javascript logic
+///front end javascript logic
 
+var id = $("#characterName").data('name')
 
 var encounterChance = Math.random()
-$(document).ready(function(){
+$(document).ready(function () {
 
     var turn = 0
-$("#next").on("click", function(){
-    turn ++
-    gameboi()
-    
+    $("#next").on("click", function () {
+        gameboi()
+        console.log(turn)
+        
+
+    })
+
+    function gameboi() {
+        switch (turn) {
+            case 0:
+                $("#textbox").html(dialogue[turn])
+                //buttons that on click change tthe turn ++ 
+
+            case (turn % 2 != 0 && turn < 9):
+                $("#textbox").html(dialogue[4])
+                
+                
+                
+
+
+            case (turn % 2 === 0 && turn!=0):
+                $("#textbox").html(dialogue[3])
+                encounter()
+
+                break;
+            case 9:
+                endGame()
+                break;
+        }
+        turn ++
+    }
 })
-gameboi()
-function gameboi(){
-switch (turn) {
-    case 0:
-        $("#textbox").html(dialogue[turn])
-       // alert('hit')
-
-       //buttons that on click change tthe turn ++ 
-
-    case (turn % 2 != 0 && turn < 9):
-        $("#textbox").html(dialogue[4])
-         gameboi()
-        
-        
-
-    case (turn % 2 === 0):
-       $("$textbox").html(dialogue[3])
-        encounter()
-        
-        break;
-    case 9:
-       endGame()
-       break;
-}
-}
-
 encounter = (assets, hungry) => {
     if (!assets, !hungry) {
         if (encounterChance < 0.33333) {
-            
+
             $("#textbox").html(dialogue[5])
         } else if (encounterChance < 0.6666) {
-            turn++
-            return dialogue[6]
+        
+            $('#textbox').html(dialogue[6])
         } else {
             ///////////MAKE THE AJAX CALL!!!!!!!!!!!!
-            $.ajax('/characters/hp/:'+id,{
-                type: 'PUT',
-                id:id,
-
-            }).then(location.reload('/game'));
-            hp = 40
+            takeDamage()
+           
+        }
+    } else if (assets) {
+        if (encounterChance < 0.6) {
             turn++
-            return dialogue[7]
+            //escape
+            $("textbox").html(dialogue[5])
+        } else {
+            turn++
+            $("textbox").html(dialogue[6])
+        }
+    } else if (hungry) {
+        if (encounterChance < 0.6) {
+            turn++
+            $("#textbox").html(dialogue[5])
+        } else {
+            turn++
+            $("#textbox").html(dialogue[7])
         }
     }
-    else if (assets) {
-       if (encounterChance < 0.6){
-           turn ++
-           //escape
-           return dialogue[5]
-       }
-       else{
-           turn ++
-           return dialogue[6]
-       }
-    }
-    else if (hungry){
-        if(encounterChance <0.6){
-            turn ++
-            return dialogue[5]
-        }
-        else{
-            turn ++
-            return dialogue[7]
-        }
-    } 
 }
 
-gameFunc = function(){
-     
+takeDamage = function(){
+    var id = $("#characterName").data('name')
+    $.ajax('/character/hp/' + id, {
+        type: 'PUT',
+        hp: 'no'
+        
+
+
+    }).then($("#textbox").html(dialogue[7]));
+    
 }
