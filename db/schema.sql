efeec17327dfead7bad9b6a637785a5bb8de895a
liliana Quinley <lilicuellar99@gmail.com>
2018-12-10 23:23:43 -0700

    create db folder

--- db/schema.sql
+++ db/schema.sql
@@ -1,21 +1,22 @@
+DROP DATABASE IF EXISTS rpgame;
 CREATE DATABASE rpgame;
 
+use rpgame;
+
 CREATE TABLE game(
-    id INT AUTO INCREMENT NOT NULL,
+    id INT auto_increment NOT NULL,
     game_name VARCHAR(140) NOT NULL,
     password VARCHAR(24) NOT NULL,
     area INT DEFAULT 0,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE character(
-    id INT AUTO INCREMENT NOT NULL,
+CREATE TABLE characters (
+    id INT auto_increment  NOT NULL,
     character_name VARCHAR(100) NOT NULL,
     gold INT DEFAULT 2000,
     assets INT DEFAULT 0,
     potion INT DEFAULT 0,
     food INT DEFAULT 0,
-    PRIMARY KEY(id),
-    FOREIGN KEY(character.id),
-);
-
+    PRIMARY KEY(id)
+)
