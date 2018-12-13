05c69282ccf9fd47ad8b6fec155295f40af55891
Max Wilets <mwilets@gmail.com>
2018-12-12 18:43:21 -0700

    frontpage

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 app.use(express.static("public"));
-app.use(express.static(__dirname + "/public"));
 app.use(method("_method"));
 
 // Handlebars
