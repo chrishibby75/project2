b92abd2a413fbe9d7e04ec4c23dc471d6f977b8e
Max Wilets <mwilets@gmail.com>
2018-12-14 13:57:36 -0700

    turning works

--- public/js/gameplay.js
+++ public/js/gameplay.js
@@ -13,93 +13,102 @@ var dialogue = [
 ///front end javascript logic
 
 //function for video game text
-var showText = function (target, message, index, interval) {   
-    if (index < message.length) {
-      $(target).append(message[index++]);
-      setTimeout(function () { showText(target, message, index, interval); }, interval);
-    }
-  }
-var id = $("#characterName").data('name')
-
-
 var encounterChance = Math.random()
+var showText = function (target, message, index, interval) {
+    if (index < message.length) {
+        $(target).append(message[index++]);
+        setTimeout(function () {
+            showText(target, message, index, interval);
+        }, interval);
+    }
+}
+var id = $("#characterName").data('name')
 $(document).ready(function () {
-    var turn = parseInt($("#turn").data('turn'))  
-$("#adventure").hide();
-$("#shop").hide();
-$('#items').hide();
-$("#run").hide();
-$("#useWeapon").hide()
-$('#buy').on('click', function(){
-    $('#items').show()
-    $("#buy").hide()
-    
-})
-    //var turn = 0
-    
-     
+    var turn = parseInt($("#turn").data('turn'))
+    $("#adventure").hide();
+    $("#shop").hide();
+    $('#items').hide();
+    $("#run").hide();
+    $("#useWeapon").hide()
+    $('#buy').on('click', function () {
+        $('#items').show()
+        $("#buy").hide()
+
+    })
+    gameboi(turn)
+
     $("#next").on("click", function () {
+        turn++
         $('#msg').empty();
-        gameboi(turn,turn++, updateTurn());
-        console.log(turn);
-        
+
+        gameboi(turn, updateTurn());
+        console.log(turn)
 
     })
 
-    $(function () {
-        showText("#msg", dialogue[turn], 0, 100);   
-      })
-    
+    //  showText("#msg", dialogue[turn], 0, 100);   
+
+
 
     function gameboi(turn) {
+        console.log('yeppers this is the turn' + turn)
         switch (turn) {
             case 0:
-            $(function () {
-                showText("#msg", dialogue[turn], 0, 100);   
-              })
+                $(function () {
+                    showText("#msg", dialogue[turn], 0, 75);
+                    turn++
+
+                })
                 //buttons that on click change tthe turn ++ 
-                
+
                 break;
             case 1:
-            $(function () {
-                $("#adventure").show()
-                $('#shop').show()
-                $('#next').hide()
-                showText("#msg", dialogue[4], 0, 100);   
-              })
-                
+                $(function () {
+                    $("#adventure").show()
+                    $('#shop').show()
+                    $('#next').hide()
+                    showText("#msg", dialogue[4], 0, 100);
+                })
+
                 break;
-                
-              case 3:
-              $("#textbox").html(dialogue[4]) 
-              break; 
-
-
             case 2:
-                $("#textbox").html(dialogue[3])
-                encounter()
-
+                showText("#msg", dialogue[3], 0, 100);
+                //encounter()
                 break;
+
+            case 3:
+                $("#textbox").html(dialogue[4])
+                break;
+
+
+
             case 9:
                 endGame()
                 break;
         }
 
-    $("#adventure").on("click", function(){
-        $("#adventure").hide()
-        $("#shop").hide()
-        $("#useWeapon").show();
-        $("#run").show()
-        $('.gameCont').css("background-image", "url('/imgf/insideCave.jpg')")
-    })
+        $("#adventure").on("click", function () {
+            console.log(turn)
+            turn++
+            $("#adventure").hide()
+            $("#msg").empty()
+            $("#shop").hide()
+            $("#useWeapon").show();
+            $("#run").show()
+            $('.gameCont').css("background-image", "url('/imgf/insideCave.jpg')")
+            console.log(turn)
+
+
+            gameboi(turn, updateTurn())
+        })
     }
 
-    updateTurn = function(){
+    updateTurn = function () {
         //turn++
         var id = $("#characterName").data('name')
-        $.ajax('/turnUpdate/'+ id+"/"+turn, {
+        $.ajax('/turnUpdate/' + id + "/" + turn, {
             type: 'PUT',
-            
+
         }).then()
     }
 })
@@ -110,11 +119,11 @@ encounter = (assets, hungry) => {
             $("#textbox").html(dialogue[5])
         } else if (encounterChance < 0.6666) {
             getGold()
-           
+
         } else {
             //if they end up taking damage
             takeDamage()
-           
+
         }
     } else if (assets) {
         if (encounterChance < 0.6) {
@@ -136,21 +145,26 @@ encounter = (assets, hungry) => {
     }
     return
 }
-getGold = function(){
+timeboi = function () {
+    var time = 0
+    console.log('timeboi')
+    time++
+}
+getGold = function () {
     var id = $('#characterName').data('name')
     $.ajax('/character/gold/' + id, {
         type: 'PUT'
-    }).then( $('#textbox').html(dialogue[6]))
-    
+    }).then($('#textbox').html(dialogue[6]))
+
 }
-takeDamage = function(){
+takeDamage = function () {
     var id = $("#characterName").data('name')
     $.ajax('/character/hp/' + id, {
         type: 'PUT',
         hp: 'no'
-        
+
 
 
     }).then($("#textbox").html(dialogue[7]));
-    
+
 }
